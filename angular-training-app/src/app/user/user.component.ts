9b3be22d76bc0d6d862a44e7097286165e2b43b8
annaivv <annashalya@gmail.com>
2025-01-26 19:54:18 +0100

    feature: inputs for UserComponent added

--- angular-training-app/src/app/user/user.component.ts
+++ angular-training-app/src/app/user/user.component.ts
@@ -1,8 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, computed, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,15 +8,45 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  // get imagePath() {
-  //   return '../../assets/users/' + this.selectedUser.avatar;
-  // }
+  // avatar = input.required<string>(); // readonly
+  // name = input.required<string>(); // readonly
 
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => 'assets/users/' + this.avatar());
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
   }
+
+  onSelectUser() {}
 }
+
+// Non-configurable UserComponent
+// import { Component, computed, signal } from '@angular/core';
+
+// import { DUMMY_USERS } from '../dummy-users';
+
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+// @Component({
+//   selector: 'app-user',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './user.component.html',
+//   styleUrl: './user.component.css',
+// })
+// export class UserComponent {
+//   selectedUser = signal(DUMMY_USERS[randomIndex]);
+//   imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+//   // get imagePath() {
+//   //   return '../../assets/users/' + this.selectedUser.avatar;
+//   // }
+
+//   onSelectUser() {
+//     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+//     this.selectedUser.set(DUMMY_USERS[randomIndex]);
+//   }
+// }
