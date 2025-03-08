9895f47c5718f105aaff8b961dc6614475016735
annaivv <annashalya@gmail.com>
2025-03-08 20:52:16 +0100

    task manager project completed

--- angular-training-app/src/main.ts
+++ angular-training-app/src/main.ts
@@ -1,5 +1,12 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+// // Approach for standalone components
+// import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppComponent } from './app/app.component';
+// import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+// bootstrapApplication(AppComponent).catch((err) => console.error(err));
+
+// Approach for modules
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
