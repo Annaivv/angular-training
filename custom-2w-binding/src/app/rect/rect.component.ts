913155a02516eed34b133e76eaaca9c2e17279bf
annaivv <annashalya@gmail.com>
2025-03-11 22:28:51 +0100

    feature: 2w binding for size with input and output added

--- custom-2w-binding/src/app/rect/rect.component.ts
+++ custom-2w-binding/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -8,9 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './rect.component.css',
 })
 export class RectComponent {
-  // Todo: Implement custom two-way binding
+  size = input.required<{ width: string; height: string }>();
+  sizeChange = output<{ width: string; height: string }>();
 
   onReset() {
-    // ...
+    this.sizeChange.emit({ width: '200', height: '100' });
   }
 }
