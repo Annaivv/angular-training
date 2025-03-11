aedff32f5e5d796f50edb4e1b186a31d8b41c34c
annaivv <annashalya@gmail.com>
2025-03-11 22:36:15 +0100

    feature: custom 2w binding with model function added

--- custom-2w-binding/src/app/rect/rect.component.ts
+++ custom-2w-binding/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, model, output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -8,10 +8,11 @@ import { Component, input, output } from '@angular/core';
   styleUrl: './rect.component.css',
 })
 export class RectComponent {
-  size = input.required<{ width: string; height: string }>();
-  sizeChange = output<{ width: string; height: string }>();
+  // size = input.required<{ width: string; height: string }>();
+  // sizeChange = output<{ width: string; height: string }>();
+  size = model.required<{ width: string; height: string }>();
 
   onReset() {
-    this.sizeChange.emit({ width: '200', height: '100' });
+    this.size.set({ width: '200', height: '100' });
   }
 }
