b411cf6275d1e889d2f2bff99a15e0e24c97e71c
annaivv <annashalya@gmail.com>
2025-04-07 23:19:31 +0200

    bugfix: complete task functionality and preserving queryParams

--- routing/src/app/users/users.routes.ts
+++ routing/src/app/users/users.routes.ts
@@ -16,7 +16,7 @@ export const routes: Routes = [
   {
     path: 'tasks',
     component: TasksComponent,
-    runGuardsAndResolvers: 'paramsOrQueryParamsChange',
+    runGuardsAndResolvers: 'always',
     resolve: {
       userTasks: resolveUserTasks,
     },
