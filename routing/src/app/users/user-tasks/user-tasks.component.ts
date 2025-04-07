842be83fd81793ed43aed8f6c8a49eb9e227cf4b
annaivv <annashalya@gmail.com>
2025-04-07 19:04:59 +0200

    feature: retrieving tasks for each user added

--- routing/src/app/users/user-tasks/user-tasks.component.ts
+++ routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent {
+  userId = input.required<string>();
+  private usersService = inject(UsersService);
+
+  userName = computed(
+    () =>
+      this.usersService.users.find((user) => user.id === this.userId())?.name
+  );
+}
